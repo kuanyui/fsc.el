174daaaac516f08a217916b080598ddee8a08338
kuanyui <azazabc123@gmail.com>
2014-07-09 00:06:40 +0800

    Rewrite fsc-vertical.el totally.

--- fsc-vertical.el
+++ fsc-vertical.el
@@ -1,117 +1,79 @@
-(defun fsc-split-sentence-to-lists (vOriginal vLineCharNum)
-  "First arg is an ONE-LINE string as inputed sentence. The
-second is 'How many characters in a vertical line', an integer.
-For example, when vLineCharNum = 3, Output is a reversed list."
-  ;; (...
-  ;;  (\"四\" \"五\" \"六\")
-  ;;  (\"一\" \"二\" \"三\"))
-  (let* ((vOriginal (fsc-convert-halfwidth-to-fullwidth vOriginal))
-         (vSplittedString (split-string-and-unquote vOriginal ""))
-         (vStart 0)
-         vOutput
-         (vSentenceLength (length vOriginal)) ;Length of input
-         (vLack (- vLineCharNum (% vSentenceLength vLineCharNum))))
-    ;; vLack is how many char lacks in the last list (use "　" to fill)
-    ;; When !=0, add "　" of vLack in the last list.
-    (dotimes (i (/ vSentenceLength vLineCharNum))
-      (push (subseq vSplittedString vStart
-                    (+ vStart vLineCharNum)) vOutput)
-      (setq vStart (+ vStart vLineCharNum)))
-    ;; Add "　"(s) into the last list to fit rectangle array.
-    (when (not (= vLack 0))
-      (let* ((TEMP-LIST (make-list vLack "　")))
-        (setq TEMP-LIST
-              (append (subseq vSplittedString vStart vSentenceLength)
-                      TEMP-LIST))
-        (push TEMP-LIST vOutput)))
-    vOutput))
+;;; fsc-verti.el --- Convert string into CJK vertical writing style   -*- lexical-binding: t; -*-
+;; http://en.wikipedia.org/wiki/Horizontal_and_vertical_writing_in_East_Asian_scripts
+;; 就是把字串轉成直書。 縦書に文字列を変換する。
+;; Copyright (C) 2014  kuanyui
+;; 
+;; Author: kuanyui <azazabc123@gmail.com>
+;; Keywords:
 
-(defun fsc-rotate-text-and-gen-list (input num)
-  "First arg is an ONE-LINE string as inputed sentence.
-The second is 'How many characters in a line', an integer.
-Output is a SINGLE-LIST containing elements of string."
-  ;;(("　" "　" "　" "李" "白" "　")
-  ;; ("低" "頭" "思" "故" "鄉" "。")
-  ;; ("舉" "頭" "望" "明" "月" "，")
-  ;; ("疑" "似" "地" "上" "霜" "，")
-  ;; ("床" "前" "明" "月" "光" "，"))
-  ;; =1=> Elements of string in single list
-  ;; ("　 低 舉 疑 床 "
-  ;;  "　 頭 頭 似 前 "
-  ;;  "　 思 望 地 明 "
-  ;;  "李 故 明 上 月 "
-  ;;  "白 鄉 月 霜 光 "
-  ;;  "　 。 ， ， ， ")
-  ;; =2=>
-  ;; 　 低 舉 疑 床
-  ;; 　 頭 頭 似 前
-  ;; 　 思 望 地 明
-  ;; 李 故 明 上 月
-  ;; 白 鄉 月 霜 光
-  ;; 　 。 ， ， ，
-  (let (FIN-LIST vInputList)
-    (setq vInputList (fsc-split-sentence-to-lists input num))
-    (dotimes (n num)         ;一行直行有num個字，n從0開始累加直到num。
-      (let (TEMP-STR)        ;產生"　 低 舉 疑 床 "作為TEMP-STR並插入到list的第n個element
-        (dolist (x vInputList) ;x依序成為一個個的list，("　" ...) ("低" ...) ("舉" ...)
-          (setf TEMP-STR (concat TEMP-STR (elt x n) " ")))  ;，並加上直行之間的空白
-        (push TEMP-STR FIN-LIST)))  ;一列列push進FIN-LIST
-    (setq FIN-LIST (reverse FIN-LIST))
-    FIN-LIST))
+(defun fsc/vertical-interactively (text number)
+  "Rotate the text vertically from minibuffer."
+  (interactive "sInput text: \nnHow many characters a line: ")
+  (insert (fsc-vertical number text)))
 
-(defun fsc-rotate-text (input num)
-  "First arg is string (single/multiple line are ok),
-second arg is how many characters a line.
-Output is rotated text."
-  ;; => split lines into elements of a list
-  ;; ("床前明月光，疑似地上霜，舉頭望明月，低頭思故鄉。　　　李白"
-  ;;  "眾裡尋他千百度；驀然回首，那人卻在燈火闌珊處。")
-  ;; element string => `fsc-rotate-text-and-gen-list' =>
-  ;; ("　 低 舉 疑 床 "
-  ;;  "　 頭 頭 似 前 "
-  ;;  "　 思 望 地 明 "
-  ;;  "李 故 明 上 月 "
-  ;;  "白 鄉 月 霜 光 "
-  ;;  "　 。 ， ， ， ")
-  ;; 一產出上面這個就立刻把他跟FIN-LIST (make-list num "")的元素一個個concat起來。
-  ;; 都concat完後，最後再一口氣把FIN-LIST的東西一項一項加上換行給print出來
-  ;; 　 低 舉 疑 床
-  ;; 　 頭 頭 似 前
-  ;; 　 思 望 地 明
-  ;; 李 故 明 上 月
-  ;; 白 鄉 月 霜 光
-  ;; 　 。 ， ， ，
-  (let* ((FIN "")
-         ;;把input的每一行切開一個個丟進list'("第一行" "第二行" ...)
-         (lInputLines (split-string-and-unquote input "\n"))
-         (FIN-LIST (make-list num "")))
-    (dolist (x lInputLines)
-      (let* ((ELT (fsc-rotate-text-and-gen-list x num)))
-        (setq FIN-LIST
-              (map 'list (lambda (a b) (concat a b))
-                   ELT FIN-LIST))))
-    (mapcar (lambda (y)
-              (setq FIN (format "%s%s\n" FIN y)))
-            FIN-LIST)
-    (setq FIN (replace-regexp-in-string " $" "" FIN)) ;去掉每行結尾空格
-    (setq FIN (replace-regexp-in-string "^　 " "" FIN))
-    FIN
-    ))
+(defun fsc/vertical-region (begin end number)
+  "Rotate the text of region to vertical. Notice that the
+original text will be replaced."
+  (interactive "r\nnHow many characters a line: ")
+  (insert (fsc-vertical number (delete-and-extract-region begin end))))
 
-(defun fsc-rotate-text-from-minibuffer ()
-  ""
-  (interactive)
-  (let* ((text (read-from-minibuffer "Input text (CJK only): "))
-         (num (string-to-int (read-from-minibuffer "How many characters a line: "))))
-    (insert (fsc-rotate-text text num))))
+(defun fsc/vertical-region-and-copy (begin end number)
+  "Rotate the text of region to vertical, and copy into kill-ring."
+  (interactive "r\nnHow many characters a line: ")
+  (kill-new (fsc-vertical number (buffer-substring-no-properties begin end)))
+  (message "String rotated to vertical and copied."))
 
+;; 1. Convert halfwidth to fullwidth
+;; 2. Split-string by newline ("第一行" "第二行")
+;; 3. Fill every string with fullwidth space to minimum mutiple of NUM. e.g. NUM=5 ("第一行　　" "第二行　　")
+;; 4. Concat string, and list all characters ("第" "一" "行" "　" "　" "第" "二" "行" "　" "　")
+;; 5. Send this list to `fsc-vertical--rotate-internal' to process.
+;; 6. mapconcat the result, and insert spaces between each vertical lines.
 
-(defun fsc-convert-halfwidth-to-fullwidth (text)
+(defun fsc-vertical (num text)
+  (let* ((fin (mapcar (lambda (x)
+                        (if (> (% (length x) num) 0)
+                            (concat x (make-string (- num (% (length x) num)) (string-to-char "　")))
+                          x))
+                      (split-string (fsc-vertical-halfwidth-to-fullwidth text) "\n")))
+         (fin (fsc-vertical--rotate-internal num
+                                             (mapcar #'string
+                                                     (string-to-list (apply #'concat fin))))))
+    (mapconcat (lambda (x)
+                 (mapconcat (lambda (y) y)
+                            x " "))
+               fin "\n")))
+
+(defun fsc-vertical--rotate-internal (num string-list)
+  "This function should be called by `fsc-vertical'
+NUM is characters amount a line.
+STRING-LIST should be a list like this (NUM = 5):
+(\"第\" \"一\" \"行\" \"　\" \"　\" \"第\" \"二\" \"行\" \"　\" \"　\")"
+  (mapcar (lambda (x) (reverse x))
+          (loop for l from 1 to num collect ; (/ (length string-list) num) is amount of lines in final output (length of each list).
+                (loop for i from 0 to (1- (/ (length string-list) num)) collect
+                      (elt string-list (+ (1- l) (* i num)))))))
+
+;; l1: 0 5 10 15 ...
+;; l2: 1 6 12 16 ...
+;; l3: 2 7 13 17 ...
+;; l4: 3 8 14 18 ...
+;; l5: 4 9 15 19 ...
+
+;; After reversal, final output:
+;; '(("第" "第")
+;;   ("二" "一")
+;;   ("行" "行")
+;;   ("　" "　")
+;;   ("　" "　"))
+
+(defun fsc-vertical-halfwidth-to-fullwidth (text)
   ""
   (mapcar (lambda (x)
             (when (string-match (car x) text)
               (setq text (replace-regexp-in-string (car x) (cdr x) text))))
-          '(("0" . "０")
+          '((" " . "　")
+            ("0" . "０")
             ("1" . "１")
             ("2" . "２")
             ("3" . "３")
@@ -194,4 +156,3 @@ Output is rotated text."
             ))
   text)
 
-(provide 'fsc-vertical)
