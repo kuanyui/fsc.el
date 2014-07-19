957b4e40ad081e03f72861989ede1471f89b07ab
kuanyui <azazabc123@gmail.com>
2014-07-19 09:41:11 +0800

    Finally rewrite.

--- fsc-rearrange.el
+++ fsc-rearrange.el
@@ -22,18 +22,23 @@
   "input should be an interger, and output a list which elements are out of order.
 Index from 1.
 For example: input is 7, output is like (5 2 6 7 4 3 1)."
-  (let ((OUTPUT '()))
-    (while (< (length OUTPUT) range-max)
-      (let* ((x (random (1+ range-max))))
-        (when (not (eq x 0))
-          (add-to-list 'OUTPUT x))))
-    OUTPUT))
+  (loop with fin = nil
+        for x = (random range-max)
+        for len = (length fin)
+        when (and (> x 0)
+                  (< len range-max)
+                  (not (= x (1+ len))))
+                  do (cl-pushnew x fin)
+        finally return fin))
+
+(loop repeat 50 collect (fsc-rearrange-random-intergers-list 2))
+
 
 (defun fsc-rearrange-random-intergers-list-paranoid (range-max)
   "Same as `fsc-rearrange-random-intergers-list', but the output list never
 sorted increasingly."
-  (let ((output (fsc-rearrange-random-intergers-list range-max))
-        (order (sort output '<)))
+  (let* ((output (fsc-rearrange-random-intergers-list range-max))
+         (order (sort output '<)))
     (if (equal output order)
         (fsc-rearrange-random-intergers-list-paranoid range-max)
       output)))
@@ -43,14 +48,20 @@ sorted increasingly."
 e.g. (fsc-sub-char \"test\" 2) => \"e\""
   (make-string 1 (aref string (1- integer))))
 
+(defvar fsc-punc-pat "\\([~!@#\\$%\\^&\\*()_\\+\\.,<>/\\?]\\)")
+
 (defun fsc-rearrange--split-sentence (input)
   "Input can be a multiple lines strings.
 This function split the input (by space) into a nested list (by newline).
 e.g. \"The first line \nThe second line\"
 => ((\"The\" \"first\" \"line\") (\"The\" \"second\" \"line\"))"
-  (setq input (replace-regexp-in-string "\\(\\cc\\)\\(\\ca\\)" "\\1 \\2" input))
-  (setq input (replace-regexp-in-string "\\(\\ca\\)\\(\\cc\\)" "\\1 \\2" input))
-  (setq input (replace-regexp-in-string "\\([。，！？；：「」『』()（）、【】《》〈〉]\\)" " \\1 " input))
+  (setq input (replace-regexp-in-string "\\(\\cc\\)\\(\\cl\\)" "\\1 \\2" input))
+  (setq input (replace-regexp-in-string "\\(\\cl\\)\\(\\cc\\)" "\\1 \\2" input))
+  (setq input (replace-regexp-in-string (concat "\\(\\cc\\)" fsc-punc-pat) " \\1 \\2  " input))
+  (setq input (replace-regexp-in-string (concat "\\(\\cl\\)" fsc-punc-pat) " \\1 \\2  " input))
+  (setq input (replace-regexp-in-string (concat fsc-punc-pat "\\(\\cc\\)") " \\1 \\2  " input))
+  (setq input (replace-regexp-in-string (concat fsc-punc-pat "\\(\\cl\\)") " \\1 \\2  " input))
+  (setq input (replace-regexp-in-string "\\([。，！？；：「」『』（）、【】《》〈〉]\\)" " \\1 " input))
   (let (output)
     (mapcar (lambda (x)
               (push (split-string-and-unquote x " +") output))
@@ -60,34 +71,29 @@ e.g. \"The first line \nThe second line\"
 
 ;; [FIXME] ...orz如果latin-word是"(parenthesis)"這樣的字串勒...需要用到 (("符號" . 位置) ...)這麼機車的方法處理嗎...
 (defun fsc-rearrange--latin (latin-word)
-  "input should be a single word and output whose characters are randomly ordered one.
-e.g. \"foobar!\" => \"fboaor!\". (If ending contains punctuation, it remains in there)"
-  (let* (input output
-         (PUNC (if (string-match "[.,?!\)]$" latin-word) ;if punctuation exist in the end of word
-                   (progn (setq input (substring-no-properties latin-word 0 (1- (length latin-word))))
-                          (fsc-sub-char latin-word (length latin-word)))
-                 (progn (setq input latin-word)
-                        nil)))
-         (INPUT_LENGTH (length input))   ;"test" => 4
-         (RAND_LIST (fsc-rearrange-random-intergers-list (length input))) ;"test" => (1 3 4 2)
-         )
-    (case INPUT_LENGTH
-      (1 input)
-      (2 input)
-      (3 (concat (fsc-sub-char input 1) (fsc-sub-char input 3) (fsc-sub-char input 2)))
-      (t
-       (setq RAND_LIST (remove INPUT_LENGTH RAND_LIST)) ;delete largest num and 1, because we want the string keeping the char order of head & tail.
-       (setq RAND_LIST (remove 1 RAND_LIST))
-       (setq output (fsc-sub-char input 1)) ;the first char of the input string. "test" => "t"
-       (dolist (x RAND_LIST)      ;get random interger from RAND_LIST
-         (setq output (concat output (fsc-sub-char input x))))
-       (setq output (concat output (fsc-sub-char input INPUT_LENGTH)))
-       (if (and (>= INPUT_LENGTH 4)     ;Ensure the word in indeed "randomed"
-                (equal input output))
-           (fsc-rearrange--latin latin-word)   ;If the same as input, do again.
-         (if PUNC
-             (format "%s%s" output PUNC)
-           output))))))
+  "input should be a single word and output whose characters are randomly ordered one (not include punctuation).
+e.g. \"foobar\" => \"fboaor\"."
+    (cond ((eq 1 (length latin-word))
+           latin-word)
+          ((eq 2 (length latin-word))
+           (apply #'string (list (aref latin-word 1) (aref latin-word 0))))
+          ((eq 3 (length latin-word))
+           (apply #'string (list (aref latin-word 0) (aref latin-word 2) (aref latin-word 1))))
+          (t
+           (let (fin)
+             (setq fin
+                   (apply #'string
+                          (append
+                           (list (aref latin-word 0))
+                           (mapcar (lambda (x) (aref latin-word x))
+                                   (fsc-rearrange-random-intergers-list (- (length latin-word) 2)))
+                           (list (aref latin-word (1- (length latin-word)))))))
+             (if (equal fin latin-word)
+                 (fsc-rearrange--latin latin-word)
+               fin)))))
+          
+(loop repeat 50 collect (fsc-rearrange--latin "Test"))
+
 
 (defun fsc-rearrange--chinese (input)
   "Input should be a chinese string (not include punctuation).
@@ -129,22 +135,10 @@ This function will decide the input should use `fsc-rearrange--latin' or
   ;; [FIXME] 記得不要動到數字的順序。
   (cond ((string-match "\\cc" input)
          (fsc-rearrange--chinese input))
-        ((string-match "\\ca" input)
+        ((string-match "\\cl" input)
          (concat (fsc-rearrange--latin input) " "))
-        ((string-match "[0-9]" input)
+        (t
          (concat input " "))))
-
-;; [FIXME] 改用mapconcat重寫，可以避免最後一行多出的換行
-;; (defun fsc-rearrange (input)
-;;   "Main function to handle sentences (include newlines, CJK/Latin)."
-;;   (let ((FIN ""))
-;;     (mapcar (lambda (x)                 ; ("The" "sentence" ".")
-;;               (let ((line ""))
-;;                 (mapcar (lambda (y)     ; "The"
-;;                           (setq line (concat line (fsc-rearrange-process-input-single y)))) x)
-;;                 (setq FIN (concat FIN line "\n"))))
-;;             (fsc-rearrange--split-sentence input)) ; (("The" "sentence" ".")("Apple"))
-;;     (replace-regexp-in-string "\\(\\cc\\)\\([A-z0-9]\\)" "\\1 \\2" FIN)))
 
 (defun fsc-rearrange (input)
   "Main function to handle sentences (include newlines, CJK/Latin)."
@@ -155,9 +149,9 @@ This function will decide the input should use `fsc-rearrange--latin' or
                  "\n") ; (("The" "sentence" ".")("Apple"))
     )
 
-
+'("This" "is" "..." "一個簡單測試" "。")
 (fsc-rearrange "WTFPL（Do What The Fuck You Want To Public License，中文譯名：你他媽的想幹嘛就幹嘛公眾授權條款）
-是一種不太常用的、極度放任的自由軟體授權條款。")
+是一種不太常用的、極度放任的自由軟體授權條款...。")
 
 
 
